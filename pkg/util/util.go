f2e7fb3c87471c9001142c6bef70e2d1435fc6a1
setrofim <setrofim@gmail.com>
2026-04-29 15:24:27 +0100

    refactor: add utility functions to guess commit type
    Encapsulate checks for for the commit type base on initial bytes into
    their own utility functions.
    
    Signed-off-by: setrofim <setrofim@gmail.com>

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -1,6 +1,9 @@
 package util
 
-import "strings"
+import (
+	"slices"
+	"strings"
+)
 
 // Normalize returns a normalized version of a name.
 func Normalize(name string) string {
@@ -8,4 +11,26 @@ func Normalize(name string) string {
 	name = strings.ToLower(name)
 	name = strings.ReplaceAll(name, "-", "_")
 	return name
+}
+
+// IsSignedCoRIM returns true if the provided buffer appears to contain a
+// signed CoRIM (no validation is performed).
+func IsSignedCoRIM(buf []byte) bool {
+	if len(buf) == 0 {
+		return false
+	}
+
+	// tag 18 -> COSE_Sign1 -> signed corim
+	return buf[0] == 0xd2
+}
+
+// IsUnignedCoRIM returns true if the provided buffer appears to contain an
+// unsigned CoRIM (no validation is performed).
+func IsUnsignedCoRIM(buf []byte) bool {
+	if len(buf) < 3 {
+		return false
+	}
+
+	// tag 501 -> unsigned corim
+	return slices.Equal(buf[:3], []byte{0xd9, 0x01, 0xf5})
 }
